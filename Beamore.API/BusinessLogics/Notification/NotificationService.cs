3a97820490cd38ed9d296024bfe60952e4a0212c
Ersin YIILDIZ <eersinyildizz@gmail.com>
2017-06-19 19:32:34 +0300

    Update NotificationService.cs

--- Beamore.API/BusinessLogics/Notification/NotificationService.cs
+++ Beamore.API/BusinessLogics/Notification/NotificationService.cs
@@ -12,12 +12,12 @@ namespace Beamore.API.BusinessLogics.Notification
         public static async void SendNotificationForAndroidAsync()
         {
             //Endpoint = sb://connectsmartnotificationhub.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=dWTA28wJlB9xm5dMlyKRApC7erVRTSVfCDw7b+onDMs=
-            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://beamorehub.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=0j3zWfrnNnFFPjOGIO3P9NZUiBTU9G2N/JbV/SwbsG0=", "beamorehub");
+            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString("key", "hubName");
             await hub.SendGcmNativeNotificationAsync("{ \"data\" : {\"message\":\"Beamore Notification test!\"}}");
         }
         public static async void SendNotificationForAppleAsync()
         {
-            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://beamorehub.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=0j3zWfrnNnFFPjOGIO3P9NZUiBTU9G2N/JbV/SwbsG0=", "beamore");
+            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString("key", "hubName");
             var alert = "{\"aps\":{\"alert\":\"Connect Smart notification test \"}}";
             await hub.SendAppleNativeNotificationAsync(alert);
         }
